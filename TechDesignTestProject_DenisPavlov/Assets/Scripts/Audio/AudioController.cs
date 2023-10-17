7126793f415c1663b9522a1889a3c32234d37dfa
Denis Pavlov <133635192+DenisPavlov0@users.noreply.github.com>
2023-10-17 17:56:44 +0700

    second commit
    Add
    - audio_group and AudioController script
    - GlobalConstants script
    - multiple animations

---
+++ TechDesignTestProject_DenisPavlov/Assets/Scripts/Audio/AudioController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioController : MonoBehaviour
+{
+    public static AudioController instance;
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource backgroundMusicSource; 
+    [SerializeField] private AudioSource soundEffectsSource; 
+    [SerializeField] private AudioSource voiceSource; 
+
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip firstSceneBackgroundMusic; 
+    [SerializeField] private AudioClip secondSceneBackgroundMusic; 
+    
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        
+    }
+
+    private void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        switch (currentScene.name)
+        {
+            case GlobalConstants.FIRST_SCENE_TAG:
+                PlayBackgroundMusic(firstSceneBackgroundMusic);
+                break;
+            case GlobalConstants.SECOND_SCENE_TAG:
+                PlayBackgroundMusic(secondSceneBackgroundMusic);
+                break;
+            default:
+                break;
+        }
+    }
+    
+    public void PlayBackgroundMusic(AudioClip clip)
+    {
+        if (backgroundMusicSource.isPlaying)
+        {
+            backgroundMusicSource.Stop();
+        }
+        backgroundMusicSource.clip = clip;
+        backgroundMusicSource.Play();
+    }
+}
---
+++ TechDesignTestProject_DenisPavlov/Assets/Scripts/Global Data/GlobalConstants.cs
@@ -0,0 +1,7 @@
+public static class GlobalConstants 
+{
+    // == Scene ==
+    public const string FIRST_SCENE_TAG = "first_scene";
+    public const string SECOND_SCENE_TAG = "second_scene";
+    
+}
